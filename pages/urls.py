893d55a795315c728743e30ba7efb00d11747564
Ejin <ejin3003@gmail.com>
2020-08-06 19:19:20 -0400

    Minor Inprovements

--- pages/urls.py
+++ pages/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from mainPage import views
+from pages import views
 
 urlpatterns = [
     path('', views.main, name='main'),
